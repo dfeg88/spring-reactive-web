9073d6ca7d19fc9e067ed1a07d03653a89245d10
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2021-01-12 21:02:36 +0000

    intial commit

---
+++ src/test/java/com/danielfegan/reactiveweb/ReactiveWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielfegan.reactiveweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
