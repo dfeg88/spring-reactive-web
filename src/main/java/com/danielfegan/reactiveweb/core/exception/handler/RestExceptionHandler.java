c6e174ae64768221e83941de006b4a2a3278edfc
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2021-01-16 16:22:59 +0000

    rename exceptionhandler

--- src/main/java/com/danielfegan/reactiveweb/core/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/danielfegan/reactiveweb/core/exception/handler/RestExceptionHandler.java
@@ -4,13 +4,14 @@ import com.danielfegan.reactiveweb.core.exception.CountriesDataException;
 import com.danielfegan.reactiveweb.core.exception.ErrorResponseJson;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class RestExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(CountriesDataException.class)
+    @ExceptionHandler(CountriesDataException.class)
     @ResponseBody
     public ResponseEntity<ErrorResponseJson> handleCountriesDataException(final CountriesDataException e) {
         return new ResponseEntity<>(new ErrorResponseJson(e.getMessage()), HttpStatus.valueOf(e.getStatus()));
